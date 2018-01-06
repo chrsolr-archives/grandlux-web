a62f6fac5c0c0110a254232337452a67d5df2fea
iamrelos <iamrelos@gmail.com>
2018-01-06 11:43:28 -0600

    Fix App Crashed on Heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 const app = require('./server/config/express');
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT || 3000, (err) => {
     if (err) throw new Error();
 
-    console.info('Application Running on port', 3000);
+    console.info('Application Running on port', process.env.PORT || 3000);
 });
