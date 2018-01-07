c28cf7c4ca08edeb4c3f38bffa2b31c14739eb6f
iamrelos <iamrelos@gmail.com>
2018-01-07 01:38:17 -0600

    Added lorem content to home page

--- server/routes/home-route.js
+++ server/routes/home-route.js
@@ -1,8 +1,5 @@
 'use strict';
 
 module.exports = (app) => {
-    app.get('/', (req, res) => res.render('partials/index', { 
-        "message": "Website Under Development", 
-        "brand" : "keypunchers.io"
-    }));
+    app.get('/', (req, res) => res.render('partials/index'));
 };
