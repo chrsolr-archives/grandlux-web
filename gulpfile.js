f714b9c0a733d5076a4c84497c36c3301da656ac
iamrelos <iamrelos@gmail.com>
2018-01-13 10:06:32 -0600

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,12 @@ const glp = require('gulp-load-plugins')({
     lazy: true
 });
 
+gulp.task('bootstrap-compile', () => {
+    return gulp.src(`./node_modules/bootstrap/scss/bootstrap.scss`)
+        .pipe(glp.sass().on('error', glp.sass.logError))
+        .pipe(gulp.dest(config.paths.CSS));
+});
+
 gulp.task('sass-compile', () => {
     return gulp.src(`${config.paths.SASS}style.scss`)
         .pipe(glp.sass().on('error', glp.sass.logError))
